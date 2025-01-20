73d2d1a267f8caebece15be947892a57b330514d
Dtsvetaev <dtsvetaev@yahoo.com>
2025-01-20 15:34:47 +0300

    Исправлены ошибки с учтом замечаний

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,33 @@
-// TODO: write code here
+const gameContainer = document.getElementById('game');
 
+gameContainer.innerHTML = '';
 
+const grid = document.createElement('div');
+grid.classList.add('grid');
+gameContainer.appendChild(grid);
 
-// for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
+for (let i = 0; i < 16; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  grid.appendChild(cell);
 }
 
-console.log("app.js included");
+const goblin = document.createElement('img');
+goblin.src = './img/goblin.png';
+goblin.classList.add('goblin');
+document.querySelector('.cell').appendChild(goblin);
+
+let previousIndex = null; 
+
+setInterval(() => {
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => (cell.innerHTML = '')); 
+
+  let randomIndex;
+  do {
+    randomIndex = Math.floor(Math.random() * cells.length); 
+  } while (randomIndex === previousIndex); 
+
+  previousIndex = randomIndex; 
+  cells[randomIndex].appendChild(goblin); 
+}, 1000);
--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,28 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        type: 'asset/resource',
-      },
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', 
-    }),
-  ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
-
